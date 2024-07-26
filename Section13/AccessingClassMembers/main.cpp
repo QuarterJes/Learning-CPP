8c9da4f7231b014dca529b7e5499638df9bced20
QuarterJes <jjjjjjjjesang@gmail.com>
2024-07-26 17:45:06 +0100

    Section13 Updates

---
+++ Section13/AccessingClassMembers/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+class Player {
+  
+  // attributes
+  std::string name;
+  int health;
+  int xp;
+
+  // methods
+  void talk(std::string);
+  bool is_dead();
+};
+
+class Account {
+  
+  // attributes
+  std::string;
+  double balance;
+
+  // methods
+  bool desposit(double);
+  bool withdraw(double);
+};
+
+int main() {
+
+  return 0;
+}
